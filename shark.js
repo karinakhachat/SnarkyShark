b666cd3136a593dc9aee9170fd37af0abdcf2b4f
Karina Khachatryan <karinakhachat@Karinas-MacBook-Air.local>
2018-08-07 10:06:48 -0400

    trying to add shark.png icon

--- shark.js
+++ shark.js
@@ -10,7 +10,7 @@ function Bird() {
   
     this.show = function() {
       fill(255);
-      ellipse(this.x, this.y, 32, 32);
+      ellipse('shark.png', 20, 20, 20 ,20);
     }
   
     this.up = function() {
